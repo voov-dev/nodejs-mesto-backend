d80d8703f27ceff95f577e058d6b9d0d780d8305
Vladimir Ostapov <35750028+voov-dev@users.noreply.github.com>
2024-06-05 02:13:31 +0300

    Feature: Updated Logers (#3)
    Co-authored-by: v.ostapov <vostapov@transportedlabs.com>

--- src/middlewares/errorLogger.ts
+++ src/middlewares/errorLogger.ts
@@ -1,10 +1,13 @@
+import 'winston-daily-rotate-file';
+
 import expressWinston from 'express-winston';
 import winston from 'winston';
 
 export const errorLogger = expressWinston.errorLogger({
   transports: [
-    new winston.transports.File({
-      filename: 'error.log',
+    new winston.transports.DailyRotateFile({
+      filename: 'logs/error-%DATE%.log',
+      datePattern: 'HH-DD-MM-YYYY',
     }),
   ],
   format: winston.format.json(),
