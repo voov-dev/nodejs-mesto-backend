feccf945777d1aa0d8b0f679503ed274bbede177
v.ostapov <vostapov@transportedlabs.com>
2024-06-05 02:12:49 +0300

    Feature: Updated Logers

--- src/middlewares/requestLogger.ts
+++ src/middlewares/requestLogger.ts
@@ -1,10 +1,13 @@
+import 'winston-daily-rotate-file';
+
 import expressWinston from 'express-winston';
 import winston from 'winston';
 
 export const requestLogger = expressWinston.logger({
   transports: [
-    new winston.transports.File({
-      filename: 'request.log',
+    new winston.transports.DailyRotateFile({
+      filename: 'logs/request-%DATE%.log',
+      datePattern: 'HH-DD-MM-YYYY',
     }),
   ],
   format: winston.format.json(),
